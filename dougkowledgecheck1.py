32c5d7d98c9bc7efc1438587d4ff1bb9d2ecf7d0
dwiley72 <105572664+dwiley72@users.noreply.github.com>
2022-05-28 19:21:28 -0400

    Add files via upload

---
+++ dougkowledgecheck1.py
@@ -0,0 +1,10 @@
+print("Hello, World!")
+
+Favorite_Dramas = ['The Sopranos', 'The Wire', 'Breaking Bad', 'Better Call Saul', 'Mad Men', 'The Americans']
+print(Favorite_Dramas[3])
+
+Favorite_Bands = {'Band1':'Soundgarden', 'Band2':'Pearl Jam', 'Band3':'Air Supply'}
+print(Favorite_Bands['Band1'])
+
+Favorite_Teams = ('Louisville Cardinals', 'Mississippi State Bulldogs', 'Miami Dolphins', 'Cincinnati Reds')
+print(Favorite_Teams[0])
